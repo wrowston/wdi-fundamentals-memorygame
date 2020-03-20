0c4444f8cbf4c50f5245875d4a6ee68e01eb8b27
wrowston <willrowston@gmail.com>
2020-03-20 11:27:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,35 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [{
+        rank : 'queen',
+        suit : 'hearts',
+        cardImage : "images/queen-of-hearts.png"
+    },
+    {
+        rank : 'queen',
+        suit : 'diamonds',
+        cardImage : "images/queen-of-diamonds.png"
+    },
+    {
+        rank : 'king',
+        suit : 'hearts',
+        cardImage : "images/king-of-hearts.png"
+    },
+    {
+        rank : 'king',
+        suit : 'diamonds',
+        cardImage : "images/king-of-diamonds.png"
+    }];
+
 const cardsInPlay = [];
 
 
 
 function flipCard(cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
 
     checkForMatch();
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 }
 
 flipCard(0);
